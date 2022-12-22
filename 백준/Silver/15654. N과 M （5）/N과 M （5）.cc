974838dba0e30db188a95c61d842bfea2668fb16
Hyeonwoo <120644862+hyeon2158@users.noreply.github.com>
2022-12-23 01:45:50 +0900

    [Silver III] Title: N과 M (5), Time: 40 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+
+int N, M;
+vector <int> num;
+vector <bool> visit(8);
+
+void graph(int curr, int cnt, queue<int> a) {
+	a.push(num[curr]);
+	if (cnt == M) {
+		while (!a.empty()) {
+			cout << a.front() << ' ';
+			a.pop();
+		}
+		cout << '\n';
+		return;
+	 }
+	else {
+		for (size_t i = 0; i < num.size(); i++)
+		{
+			if (visit[i] == true) continue;
+			visit[i] = true;
+			graph(i, cnt + 1, a);
+			visit[i] = false;
+		}
+	}
+}
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> M;
+
+	for (size_t i = 0; i < N; i++)
+	{
+		int ent;
+		cin >> ent;
+		num.push_back(ent);
+	}
+	sort(num.begin(), num.end());
+	for (size_t i = 0; i < N; i++)
+	{
+		queue<int> a;
+		visit[i] = true;
+		graph(i, 1, a);
+		visit[i] = false;
+	}
+	return 0;
+}
