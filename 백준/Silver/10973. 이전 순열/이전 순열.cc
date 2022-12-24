4fc663ad48d5d4037d187a685702b87ae0c731af
Hyeonwoo <120644862+hyeon2158@users.noreply.github.com>
2022-12-24 10:16:52 +0900

    [Silver III] Title: 이전 순열, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/10973. 이전 순열/이전 순열.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	cin >> N;
+	vector<int> seq;
+	for (size_t i = 0; i < N; i++)
+	{
+		int ent;
+		cin >> ent;
+		seq.push_back(ent);
+	}
+	bool pos = prev_permutation(seq.begin(), seq.end());
+	if (!pos) cout << -1;
+	else {
+		for (size_t i = 0; i < N; i++)
+		{
+			cout << seq[i] << ' ';
+		}
+	}
+	return 0;
+}
