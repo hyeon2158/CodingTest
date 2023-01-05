95b3ef4225f36a272b4207f2f0ef7e36f3148d26
Hyeonwoo <120644862+hyeon2158@users.noreply.github.com>
2023-01-05 13:45:19 +0900

    [Gold III] Title: Guess, Time: 900 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1248. Guess/Guess.cc
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+typedef unsigned long long ong;
+
+int n;
+vector<vector <char> > sign(10, vector<char>(10));
+vector<int> res;
+bool decode = 1;
+
+//default cnt = 0, curr = null, sum = 0, num = i
+void DFS(int cnt, vector<int> curr, vector<int> sum, int num) { 
+	if (!decode) return;
+	if (cnt == n) {
+		res = curr;
+		decode = 0;
+		return;
+	}
+	if (sign[cnt][cnt] == '+')
+	{
+		curr.push_back(num);
+	}
+	if (sign[cnt][cnt] == '-')
+	{
+		curr.push_back(num*(-1));
+	}
+	if (sign[cnt][cnt] == '0')
+	{
+		curr.push_back(0);
+	}
+	sum.push_back(sum.back()+curr.back());
+	for (size_t i = 0; i < cnt; i++)
+	{
+		if (sign[i][cnt] == '+') {
+			if (sum[cnt+1] - sum[i] <= 0) {
+				return;
+			}
+		}
+		if (sign[i][cnt] == '-') {
+			if (sum[cnt+1] - sum[i] >= 0) {
+				return;
+			}
+		}
+		if (sign[i][cnt] == '0') {
+			if (sum[cnt+1] - sum[i] != 0) {
+				return;
+			}
+		}
+	}
+	for (size_t i = 1; i <= 10; i++)
+	{
+		DFS(cnt + 1, curr, sum, i);
+	}
+}
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n;
+	for (size_t i = 0; i < n; i++)
+	{
+		for (size_t j = i; j < n; j++)
+		{
+			cin >> sign[i][j];
+		}
+	}
+	for (size_t i = 1; i <= 10; i++)
+	{
+		vector<int> curr;
+		vector<int> sum(1);
+		DFS(0, curr, sum , i);
+	}
+	for (size_t i = 0; i < n; i++)
+	{
+		cout << res[i] << ' ';
+	}
+	return 0;
+}
